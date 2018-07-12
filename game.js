0864e4afcbff16b34078bce6c73f3e879889cd20
DavidZ <david.zafirovic@gmail.com>
2018-07-12 11:37:56 -0400

    Taking different approach

--- game.js
+++ game.js
@@ -4,64 +4,102 @@ const buttonMJ = document.querySelector('.mj')
 const buttonBron = document.querySelector('.bron')
 const buttonKobe = document.querySelector('.kobe')
 const buttonNext = document.querySelector('.next')
-var score = 0
 
-var quiz =
-             ["Which player has more scoring titles?",
-              "Which player averaged more PPG in the Playoffs?",
-              "Which player made more All-NBA First Teams?",
-              "Which player played in more All-Star Games?",
-              "Which player averaged more assists per game?",
-              "Which player never played with Shaquille O' Neil?",
-              "Which player starred in Space Jam?",
-              "Which player played more regular season games?"]
+mj0
+bron1
+kobe2
 
 
-
-var rand = quiz[Math.floor(Math.random() * quiz.length)];
-
-
-buttonStart.addEventListener('click', function(event) {
-  document.getElementById('questiontime').innerText= rand;
-})
-buttonNext.addEventListener('click', function(event) {
-  document.getElementById('questiontime').innerText = rand;
-})
-
-buttonMJ.addEventListener('click', function(event) {
-  if(rand = quiz[0], quiz[1], quiz[5]){
-
-alert("correct" + " " + score)
-
-  }
-  else {
-    return false
-  }
-
-})
-
-buttonBron.addEventListener('click', function(event) {
-  if(rand = quiz[4], quiz[6], quiz[7]){
-
-alert ("correct" + " " + score)
-
-  }
-  else {
-    return false
-  }
-
-})
-
-buttonKobe.addEventListener('click', function(event) {
-  if(rand = quiz[2], quiz[3]){
-alert("correct" + " " + score)
-
-  }
-else {
-  return false
-}
-})
-
+var quizz = [{
+  question: "Which player has more scoring titles?",
+  choices: ["Michael Jordan", "Kobe Bryant", "Lebron James"],
+  answer : 0
+},
+  {
+    question: "Which player averaged more PPG in the Playoffs?",
+    choices: [buttonMJ, buttonBron, buttonKobe],
+    answer : 0
+  },
+  {
+    question: "Which player made more All-NBA First Teams?",
+    choices: [buttonMJ, buttonBron, buttonKobe],
+    answer : 2
+  },
+  {
+    question: "Which player played in more All-Star Games?",
+    choices: [buttonMJ, buttonBron, buttonKobe],
+    answer : 2
+  },
+  {
+    question: "Which player averaged more assists per game?",
+    choices: [buttonMJ, buttonBron, buttonKobe],
+    answer : 1
+  },
+  {
+    question: "Which player never played with Shaquille O' Neil?",
+    choices: [buttonMJ, buttonBron, buttonKobe],
+    answer : 2
+  },
+  question: "Which player starred in Space Jam?",
+  choices: [buttonMJ, buttonBron, buttonKobe],
+  answer : 0
+},
+question: "Which player played more regular season games?",
+choices: [buttonMJ, buttonBron, buttonKobe],
+answer : 2
+}]
+// var quiz =
+//              ["Which player has more scoring titles?",
+//               "Which player averaged more PPG in the Playoffs?",
+//               "Which player made more All-NBA First Teams?",
+//               "Which player played in more All-Star Games?",
+//               "Which player averaged more assists per game?",
+//               "Which player never played with Shaquille O' Neil?",
+//               "Which player starred in Space Jam?",
+//               "Which player played more regular season games?"]
+//
+// var MJ = {firstq: quiz[0], secondq:quiz[1], thirdq: quiz[5]}
+// var LBJ = {fourthq: quiz[4], fifthq: quiz[6], sixthq: quiz[7]}
+// var KB = {seventhq: quiz[2], eigthq: quiz[3]}
+//
+//
+//
+// function askQuestion () {
+//
+// var rand = quiz[Math.floor(Math.random() * quiz.length)]
+//
+// buttonStart.addEventListener('click', function(event) {
+//   document.getElementById('questiontime').innerText = rand
+// })
+//
+// buttonNext.addEventListener('click', function(event) {
+//   document.getElementById('questiontime').innerText = rand
+// })
+//
+// }
+// askQuestion()
+// // if (rand = quiz[0], quiz[1], quiz[5]){
+// //   buttonMJ.addEventListener('click', function(event){
+// //
+// //   })
+// // }
+//
+//
+// buttonMJ.addEventListener('click', function(event) {
+//   if(rand = quiz[0], quiz[1], quiz[5]){
+// }
+// })
+//
+// buttonBron.addEventListener('click', function(event) {
+//   if(rand = quiz[4], quiz[6], quiz[7]){
+// }
+// })
+//
+// buttonKobe.addEventListener('click', function(event) {
+//   if(rand = quiz[2], quiz[3]){
+//   }
+// })
+//
 
 
 
